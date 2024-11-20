dbf34a31897b23c3b8734e0b3b08d352033d650c
Aditya Sharma <adityasharma96655@gmail.com>
2024-11-20 14:39:32 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+// Javascript arithmetic operators and if else statement and also the assignment operators.
+// Making driving license eligible program.
+
+let userName='Aditya Sharma';
+let ageOfYear=2008;
+const verify = 18;
+let verify_age;
+verify_age=2025-ageOfYear;
+
+
+if(verify_age>=verify){
+    console.log("You are eligible");
+}
+else{
+    console.log("Minimum age should be 18");
+}
