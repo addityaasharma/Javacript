0f4bdaf92e851e645261b8ae008eee2bcd3dfaca
Aditya Sharma <adityasharma96655@gmail.com>
2024-11-21 21:11:03 +0530

    Update for-loop.js

--- for-loop.js
+++ for-loop.js
@@ -1,4 +1,4 @@
-// making multiple of two using "for" loops
+// making multiple of two using "for" loops because i had learn today for loop in java
 j=1;
 for(i=1;i<=10;i++){
     console.log(`2 * ${j*i} is ${i*2}`);
